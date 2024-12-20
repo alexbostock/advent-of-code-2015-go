da3fd14cef6bcc1439adace0bb349eaec3f6969a
Alex Bostock <alex@bostock.uk>
2024-12-20 16:00:30 +0000

    Solve day 1

--- p1/p1.go
+++ p1/p1.go
@@ -5,16 +5,48 @@ import (
 )
 
 type Puzzle1 struct {
+	instructions string
 }
 
 func (p *Puzzle1) LoadInput(input io.ReadSeeker) error {
+	buffer, err := io.ReadAll(input)
+	if err != nil {
+		return err
+	}
+	p.instructions = string(buffer)
 	return nil
 }
 
 func (p *Puzzle1) SolvePart1() interface{} {
-	return 0
+	return countFloors(p.instructions)
 }
 
 func (p *Puzzle1) SolvePart2() interface{} {
-	return 0
+	return firstEntersBasementAt(p.instructions)
+}
+
+func countFloors(instructions string) (count int) {
+	for _, instruction := range []rune(instructions) {
+		if instruction == '(' {
+			count++
+		} else {
+			count--
+		}
+	}
+	return
+}
+
+func firstEntersBasementAt(instructions string) int {
+	currentFloor := 0
+	for index, instruction := range []rune(instructions) {
+		if instruction == '(' {
+			currentFloor++
+		} else {
+			currentFloor--
+			if currentFloor == -1 {
+				return index + 1
+			}
+		}
+	}
+	panic("No solution")
 }
--- p1/p1_test.go
+++ p1/p1_test.go
@@ -1,1 +1,19 @@
 package p1
+
+import "testing"
+
+func TestCountFloors(t *testing.T) {
+	got := countFloors("(()(()(")
+	expected := 3
+	if expected != got {
+		t.Errorf("expected %d, got %d", expected, got)
+	}
+}
+
+func TestFirstEntersBasementAt(t *testing.T) {
+	got := firstEntersBasementAt("()())")
+	expected := 5
+	if expected != got {
+		t.Errorf("expected %d, got %d", expected, got)
+	}
+}
